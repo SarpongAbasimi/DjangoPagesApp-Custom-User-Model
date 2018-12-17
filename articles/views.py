160c0668ef08410c6f9867e7f9e8b08ff4d355c6
sarpong <abasimisarpong@rocketmail.com>
2018-12-17 18:09:18 +0000

    I have added a loginRequired mixin to views and a dispatch method to delete and update views

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin 
 from django.views.generic import ListView,DetailView,DeleteView,UpdateView,CreateView
 from .models import Articles
 from django.urls import reverse_lazy
@@ -8,26 +10,44 @@ class ArticleListView(ListView):
     model = Articles
     template_name = 'article_list.html'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin,DetailView):
     model = Articles
     template_name = 'article_detail.html'
+    login_url = 'login'
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
     model =Articles
     template_name ='article_delete.html'
     success_url=reverse_lazy('article_list')
+    login_url = 'login'
+
+    def dispatch(self,request, *args, **kwargs):
+        obj=self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        else:
+            return super().dispatch(request,*args,**kwargs)
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
     model=Articles
     fields= ('title','content')
     template_name='article_edit.html'
+    login_url = 'login'
 
-class ArticleCreateView(CreateView):
+    def dispatch(self,request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied 
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model = Articles
     template_name = 'article_new.html'
     fields = ('title', 'content',)
+    login_url = 'login'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
