b59252b298daedacebfb803d6f78bf71e16693df
sarpong <abasimisarpong@rocketmail.com>
2018-12-18 14:00:35 +0000

    newspaper appp completed

--- articles/models.py
+++ articles/models.py
@@ -8,13 +8,15 @@ class Articles(models.Model):
     content=models.TextField()
     time =models.DateField(auto_now_add=True)
     author=models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-
+     
+     """ This enusres that the Atticle models doesn't have to end with another s """
     class Meta:
         verbose_name_plural='Articles'
 
     def __str__(self):
         return self.title
 
+    """ This is to tell django how to find the url of the Article model object """
     def get_absolute_url(self):
         return reverse('article_detail',args=([str(self.id)]))
 
